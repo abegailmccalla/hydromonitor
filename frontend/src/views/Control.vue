440fdd3148b6ce71dbdeab8245bdcab44437f81c
abegailmccalla <abegailtatyanamccalla7@gmail.com>
2024-02-03 08:46:53 -0500

    Hardware Section Complete

---
+++ frontend/src/views/Control.vue
@@ -0,0 +1,79 @@
+<template>
+    <v-container class="container" fluid align="center" justify="center">
+        <v-row class="row bg-surfaceVariant" justify="center">
+            <v-col class="col col1" align="center"> 
+                <v-sheet class="rounded-t-lg mb-1" color="surface" elevation="0" max-width="800" width="100%">
+                    <v-card title="LED Controls" color="surface" subtitle="Recent settings" variant="tonal" flat>
+                    </v-card>
+                </v-sheet>
+                <v-sheet clas="mb-1" color="surface" elevation="0" max-width="800" width="100%">
+                    <v-card class="pt-5" color="surface" variant="tonal">
+                        <v-slider class="pt-2 bg-surface" append-icon="mdi:mdi-car-light-high" density="compact" thumb-size="16" color="secondary" label="Brightness" direction="horizontal"  min="0" max="250" step="10" show-ticks thumb-label="always">
+                        </v-slider>
+                    </v-card>
+                </v-sheet>
+                <v-sheet class="mb-1" justify="center" align="center" color="surface" elevation="0" max-width="800" width="100%">
+                    <v-card class="pt-5" color="surface" variant="tonal" flat>
+                        <v-slider class="pt-2 bg-surface" append-icon="mdi:mdi-led-on" density="compact" thumb-size="16" color="secondary" label="LED Nodes" direction="horizontal"  min="1" max="7" step="1" show-ticks thumb-label="always">
+                        </v-slider>
+                    </v-card>
+                </v-sheet>
+                <v-sheet class="mb-1 pa-2 border" justify="center" align="center" color="surface" elevation="0" max-width="800" width="100%">
+                    <v-progress-circular rotate="0" size="200" width="15">
+                    </v-progress-circular>
+                </v-sheet>
+            </v-col>
+            <v-col class="col col2" align="center"> 
+                <v-colorpicker>
+                </v-colorpicker>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup>
+/** JAVASCRIPT HERE */
+
+// IMPORTS
+import { ref,reactive,watch ,onMounted,onBeforeUnmount,computed } from "vue";  
+import { useRoute ,useRouter } from "vue-router";
+ 
+ 
+// VARIABLES
+const router      = useRouter();
+const route       = useRoute();  
+
+
+// FUNCTIONS
+onMounted(()=>{
+    // THIS FUNCTION IS CALLED AFTER THIS COMPONENT HAS BEEN MOUNTED
+});
+
+
+onBeforeUnmount(()=>{
+    // THIS FUNCTION IS CALLED RIGHT BEFORE THIS COMPONENT IS UNMOUNTED
+});
+
+
+</script>
+
+
+<style scoped>
+/** CSS STYLE HERE */
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Symbols+2&display=swap');
+.digit {
+    font-family: 'Noto Sans Symbols 2'; 
+    font-size: 250px;
+}
+.container{
+    height: 100%; 
+    /** border: 1px solid blue; */
+}
+.row{
+    max-width: 1200px; 
+    margin: 10px 0px;
+    padding: 10px; 
+    /** border: 1px solid purple; */
+}
+</style>
+  
--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -9,7 +9,9 @@
 #include <ctype.h>
 
 // ADD YOUR IMPORTS HERE
-
+#include "DHT.h"
+#include <map>
+#include <FastLED.h>
 
 
 #ifndef _WIFI_H 
@@ -38,22 +40,25 @@
 #define ARDUINOJSON_USE_DOUBLE      1 
 
 // DEFINE THE CONTROL PINS FOR THE DHT22 
+#define DHTPIN 22
+#define DHTTYPE DHT22
 
+#define NUM_LEDS 7
+#define DATA_PIN 2
+#define CLOCK_PIN 13
 
-
+// Define the array of leds
+CRGB LEDs[NUM_LEDS];
 
 // MQTT CLIENT CONFIG  
-static const char* pubtopic      = "620012345";                    // Add your ID number here
-static const char* subtopic[]    = {"620012345_sub","/elet2415"};  // Array of Topics(Strings) to subscribe to
-static const char* mqtt_server   = "local";         // Broker IP address or Domain name as a String 
+static const char* pubtopic      = "620157646";                    // Add your ID number here
+static const char* subtopic[]    = {"620157646_sub","/elet2415"};  // Array of Topics(Strings) to subscribe to
+static const char* mqtt_server   = "www.yanacreations.com";         // Broker IP address or Domain name as a String 
 static uint16_t mqtt_port        = 1883;
 
 // WIFI CREDENTIALS
-const char* ssid       = "YOUR_SSID";     // Add your Wi-Fi ssid
-const char* password   = "YOUR_PASSWORD"; // Add your Wi-Fi password 
-
-
-
+const char* ssid       = "MonaConnect";     // Add your Wi-Fi ssid
+const char* password   = ""; // Add your Wi-Fi password 
 
 // TASK HANDLES 
 TaskHandle_t xMQTT_Connect          = NULL; 
@@ -81,7 +86,7 @@ double calcHeatIndex(double Temp, double Humid);
 
 
 /* Init class Instances for the DHT22 etcc */
- 
+DHT dht(DHTPIN, DHTTYPE);
   
 
 //############### IMPORT HEADER FILES ##################
@@ -100,6 +105,7 @@ void setup() {
   Serial.begin(115200);  // INIT SERIAL  
 
   // INITIALIZE ALL SENSORS AND DEVICES
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(LEDs, NUM_LEDS);  // GRB ordering is assumed
   
   /* Add all other necessary sensor Initializations and Configurations here */
 
@@ -143,28 +149,34 @@ void vUpdate( void * pvParameters )  {
    
           // 1. Read Humidity and save in variable below
           double h = 0;
+          h = dht.readHumidity();
            
           // 2. Read temperature as Celsius   and save in variable below
-          double t = 0;    
- 
+          double t = 0;  
+          t = dht.readTemperature();  
 
           if(isNumber(t)){
               // ##Publish update according to ‘{"id": "student_id", "timestamp": 1702212234, "temperature": 30, "humidity":90, "heatindex": 30}’
 
               // 1. Create JSon object
-              
+              StaticJsonDocument<768> doc;
+ 
               // 2. Create message buffer/array to store serialized JSON object
+              char message[800]  = {0};
               
               // 3. Add key:value pairs to JSon object based on above schema
+              doc["id"] = 620157646;
+              doc["timestamp"]  = getTimeStamp();
+              doc["temperature"]  = t;
+              doc["humidity"]  = h;
+              doc["heatindex"]  = calcHeatIndex(convert_Celsius_to_fahrenheit(t), h);
 
               // 4. Seralize / Covert JSon object to JSon string and store in message array
-               
+              serializeJson(doc, message);    
+
               // 5. Publish message to a topic sobscribed to by both backend and frontend                
-
-          }
-
-          
-            
+              publish(pubtopic, message);
+          }   
         vTaskDelay(1000 / portTICK_PERIOD_MS);  
     }
 }
@@ -205,16 +217,32 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
 
 
-  // PROCESS MESSAGE
+  // PROCESS MESSAGE: ‘{"type": "controls", "brightness": 255, "leds": 7, "color": { "r": 255, "g": 255, "b": 255, "a": 1 } }
   const char* type = doc["type"]; 
 
   if (strcmp(type, "controls") == 0){
     // 1. EXTRACT ALL PARAMETERS: NODES, RED,GREEN, BLUE, AND BRIGHTNESS FROM JSON OBJECT
+      const int brightness = doc["brightness"];
+      const int leds = doc["leds"];
+      std::map<char, int> color = doc["color"];
 
     // 2. ITERATIVELY, TURN ON LED(s) BASED ON THE VALUE OF NODES. Ex IF NODES = 2, TURN ON 2 LED(s)
+    for(int x=0; x<leds; x++){ 
+      leds[x] = CRGB( color["r"], color["g"], color["b"]); // R, G, B range for each value is 0 to 255
+      FastLED.setBrightness( brightness ); // Ranges from 0 to 255
+      FastLED.show(); // Send changes to LED array
+      delay(50);
+    }
+    delay(1000);
 
     // 3. ITERATIVELY, TURN OFF ALL REMAINING LED(s).
-   
+    for(int x=0; x<leds; x++){ 
+      leds[x] = CRGB::Black; 
+      FastLED.setBrightness( brightness );
+      FastLED.show(); 
+      delay(50);
+    }
+    delay(1000);
   }
 }
 
@@ -238,17 +266,36 @@ bool publish(const char *topic, const char *payload){
 
 //***** Complete the util functions below ******
 
+// Celsius to Fahrenheit Conversion Formula: Multiply the °C temperature by 1.8. Add 32 to this number. This is the answer in °F.
 double convert_Celsius_to_fahrenheit(double c){    
-    // CONVERTS INPUT FROM °C TO °F. RETURN RESULTS     
+    // CONVERTS INPUT FROM °C TO °F. RETURN RESULTS 
+    return (c * 1.8) + 32; 
 }
 
+// Fahrenheit to Celsius Conversion Formula: Subtract 32 from the °F temperature. Multiply this number by 5/9. This is the answer in °C.
 double convert_fahrenheit_to_Celsius(double f){    
-    // CONVERTS INPUT FROM °F TO °C. RETURN RESULT    
+    // CONVERTS INPUT FROM °F TO °C. RETURN RESULT 
+    return ((f - 32) * 5) / 9;
 }
 
+// HI= c1+c2T+c3R+c4TR+c5T2+c6R2+c7T2R+c8TR2+c9T2R2
+// In this formula,
+// HI = heat index in degrees Fahrenheit
+// R = Relative humidity
+// T = Temperature in ∘F
+// c1 = -42.379
+// c2= -2.04901523
+// c3 = -10.14333127
+// c4 = -0.22475541
+// c5 = -6.83783 x 10−3
+// c6 = -5.481717 x 10−2
+// c7 = -1.22874 x 10−3
+// c8 = 8.5282 x 10−4
+// c9 = -1.99 x 10−6
 double calcHeatIndex(double Temp, double Humid){
     // CALCULATE AND RETURN HEAT INDEX USING EQUATION FOUND AT https://byjus.com/heat-index-formula/#:~:text=The%20heat%20index%20formula%20is,an%20implied%20humidity%20of%2020%25
-  
+    //dht.computeHeatIndex(Temp, Humid, true); //The last argument, 'true,' changes the output to Fahrenheit.
+    return -42.379 + (-2.04901523 * Temp) + (-10.14333127 * Humid) + (-0.22475541 * Temp * Humid) + (-0.00683783 * Temp * 2) + (-0.05481717 * Humid * 2) + (-0.00122874 * Temp * 2 * Humid) + (0.00085282 * Temp * Humid * 2) + (-0.00000199 * Temp * 2 * Humid * 2);
 }
  
 
